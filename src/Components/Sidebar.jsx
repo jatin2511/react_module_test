15a4cefbd02c28d84296edaa5d2d8c4c53e6e0f2
jatin2511 <jatinsiwach2501@gmail.com>
2024-06-12 10:42:01 +0530

    fifth commit

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -34,13 +34,13 @@ function Sidebar() {
   return (
     <div className='h-full w-full overflow-hidden'>
       <div className='text-3xl md:font-semibold mx-10 py-5 font-[450]'>Pocket Notes</div>
-      <div className='bg-black md:text-2xl text-xl w-max h-10 text-white mx-auto md:mt-5 px-6 py-1 rounded-full font-medium' onClick={handleAddNote}>
+      <div className='bg-black md:text-2xl text-xl w-max h-10 text-white mx-auto md:mt-5 px-6 py-1 rounded-full font-medium cursor-pointer' onClick={handleAddNote}>
         + Create Notes group
       </div>
       <div className='w-full md:py-10 py-5'>
         {Object.keys(Notes).length > 0 ? (
           Object.entries(Notes).map(([key, note]) => (
-            <div key={key} className='flex w-full p-2 ml-5 items-center border-2 rounded-l-3xl cursor-pointer' onClick={(e)=>{handledisplaynote(e.target.key)}}>
+            <div key={key} className='flex w-full p-2 ml-5 items-center border-2 rounded-l-3xl cursor-pointer' onClick={()=>{handledisplaynote(key)}}>
               <div className='mr-2 h-10 w-10 rounded-[50%] flex justify-center items-center text-2xl text-white font-medium md:h-14 md:w-14 md:text-3xl' style={{backgroundColor:note.color}}>{key.substr(0,2)}</div>
               <div className='text-2xl md:text-3xl font-normal'>{key}</div>
             </div>
